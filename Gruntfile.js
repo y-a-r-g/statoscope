2e7794d34390ce42f1826ea77859596fc3ae2836
Sergey Dvoynikov <svdvoynikov@gmail.com>
2015-02-06 15:59:45 +0500

    Project restructurisation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,135 @@
-module.exports = require("./frontend/Gruntfile.js");
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        path: {
+            scripts: "./scripts",
+            styles: "./styles",
+            resources: "./resources",
+            pages: "./pages",
+            build: "./public"
+        },
+
+        clean: {
+            options: { force: true },
+            debug: ["<%= path.build %>"]
+        },
+
+        mkdir: {
+            debug: {
+                options: {
+                    create: ["<%= path.build %>"]
+                }
+            }
+        },
+
+        typescript: {
+            debug: {
+                src: ["<%= path.scripts %>/**/*.ts"],
+                dest: "<%= path.build %>/scripts.js",
+                options: {
+                    module: "amd", //or commonjs
+                    target: "es5",
+                    sourceMap: true,
+                    declaration: true,
+                    basePath: "<%= path.scripts %>/"
+                }
+            }
+        },
+
+        less: {
+            debug: {
+                options: {
+                    paths: "<%= path.styles %>/**/*.inc.less",
+                    compress: false,
+                    ieCompat: false,
+                    sourceMap: true,
+                    sourceMapURL: "styles.css.map"
+                },
+                files: {
+                    "<%= path.build %>/styles.css": "<%= path.styles %>/**/*.inc.less"
+                }
+            }
+        },
+
+        copy: {
+            debug: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "<%= path.resources %>/",
+                        src: "**/*",
+                        dest: "<%= path.build %>/"
+                    },
+                    {
+                        expand: true,
+                        cwd: "<%= path.pages %>/",
+                        src: "**/*",
+                        dest: "<%= path.build %>/"
+                    }
+                ]
+            }
+        },
+
+        watch: {
+            options: { livereload: false },
+            debug: {
+                files: [
+                    "<%= path.scripts %>/**/*.ts",
+                    "<%= path.styles %>/**/*.less",
+                    "<%= path.resources %>/**/*",
+                    "<%= path.pages %>/**/*"
+                ],
+                tasks: ["build-debug"],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+
+        express: {
+            debug: {
+                options: {
+                    script: "server.js",
+                    background: true,
+                    port: 3000
+                }
+            }
+        },
+
+        env: {
+            debug: {
+                DEBUG: "1"
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-typescript");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-express-server");
+    grunt.loadNpmTasks("grunt-env");
+    grunt.loadNpmTasks("grunt-mkdir");
+
+    grunt.registerTask("default", ["build-debug"]);
+
+    grunt.registerTask("build-debug", [
+        "clean:debug",
+        "mkdir:debug",
+        "typescript:debug",
+        "less:debug",
+        "copy:debug"
+    ]);
+
+    grunt.registerTask("build-dev", [
+        "build-debug"
+    ]);
+
+    grunt.registerTask("watch-debug", [
+        "build-debug",
+        "env:debug",
+        "express:debug",
+        "watch:debug"
+    ]);
+};
--- server.js
+++ server.js
@@ -4,10 +4,11 @@ var express = require("express"),
     errorHandler = require("errorhandler"),
     methodOverride = require("method-override"),
     hostname = process.env.HOSTNAME || "0.0.0.0",
-    port = parseInt(process.env.PORT, 10) || 80;
+    port = parseInt(process.env.PORT, 10) || 80,
+    debug = !!process.env.DEBUG;
 
 app.get("/", function (req, res) {
-    res.redirect("/index.html");
+    res.redirect(debug ? "/public/index.html" : "/index.html");
 });
 
 app.use(methodOverride());
@@ -16,7 +17,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use(express.static(__dirname + "/frontend/public"));
+app.use(express.static(__dirname + (debug ? "" : "/public")));
 app.use(errorHandler({
     dumpExceptions: true,
     showStack: true
